cec522bc063d87cf4fec5b068a2029f813f54e66
timo067 <aliosman.aliosmann@gmail.com>
2024-11-03 21:26:45 +0200

    creating entities

---
+++ YuGiOhDeckEditor/Core/Entities/CardsInfo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class CardsInfo:BaseID
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public string Attribute { get; set; }
+
+        public string Typing { get; set; }
+
+        public string LevelOrRank { get; set; }
+
+        public string Archetype { get; set; }
+
+        public string TCGDate { get; set; }
+
+        public string OCGDate { get; set; }
+        
+        public string Description { get; set; }
+
+        public string PendulumDescription { get; set; }
+
+        public int AttackPoints { get; set; }
+
+        public int DefencePoints { get; set; }
+
+        public int Limit {  get; set; }
+
+        public CardsInfo(string name, string type, string attribute, string typing, string levelOrRank, string archetype, string tCGData, string oCGDate, string description, string pendulumDescription, int attackPoints, int defencePoints, int limit)
+        {
+            Name = name;
+            Type = type;
+            Attribute = attribute;
+            Typing = typing;
+            LevelOrRank = levelOrRank;
+            Archetype = archetype;
+            TCGDate = tCGData;
+            OCGDate = oCGDate;
+            Description = description;
+            AttackPoints = attackPoints;
+            DefencePoints = defencePoints;
+            Limit = limit;
+        }
+    }
+}
---
+++ YuGiOhDeckEditor/Core/Entities/DeckInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class DeckInfo:BaseID
+    {
+        public string Name { get; set; }
+
+        public string OwnerId { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime UpdatedDeck { get; set; }
+
+        public DeckInfo(string name, string ownerId, string description, DateTime dateCreated, DateTime updatedDeck)
+        {
+            Name = name;
+            OwnerId = ownerId;
+            Description = description;
+            DateCreated = dateCreated;
+            UpdatedDeck = updatedDeck;
+        }
+    }
+}

4c20caa60683119da2594f6f338b113e9755e63d
timo067 <aliosman.aliosmann@gmail.com>
2024-11-04 00:38:09 +0200

    added more entities

---
+++ YuGiOhDeckEditor/Core/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace YuGiOhDeckEditor.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } // Example entity
+        public DbSet<CardsInfo> CardsInfo { get; set; } // Example entity
+        public DbSet<DeckInfo> DeckInfo { get; set; } // Example entity
+        public DbSet<BanList> BanList { get; set; } // Example entity
+        public DbSet<UserDecks> UserDecks { get; set; } // Example entity
+        public DbSet<CardType> CardType { get; set; } // Example entity
+        public DbSet<DeckCard> DeckCard { get; set; } // Example entity
+    }
+}
---
+++ YuGiOhDeckEditor/Core/Entities/DeckCard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class DeckCard:BaseID
+    {
+        public int DeckId { get; set; } // Reference to the Deck containing the card.
+
+        public int CardId { get; set; } // Reference to the Card in the deck.
+
+        public int Quantity { get; set; } // Number of copies of this card in the deck.
+
+        public DeckCard(int deckId, int cardId, int quantity)
+        {
+            DeckId = deckId;
+            CardId = cardId;
+            Quantity = quantity;
+        }
+    }
+}
